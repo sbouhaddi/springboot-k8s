796998c67bbd278d964968abcf474296a991f067
sbouhaddi <sabri.bouhaddi@gmail.com>
2024-10-05 23:37:02 +0200

    CI github actions

---
+++ k8s-api/src/test/java/dev/sabri/k8s/K8sSampleApplicationTests.java
@@ -0,0 +1,11 @@
+package dev.sabri.k8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sSampleApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
