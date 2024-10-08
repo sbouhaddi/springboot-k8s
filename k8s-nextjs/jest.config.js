6da3ddedcefd76a126348f31da2c83e9a87fcbb3
sbouhaddi <sabri.bouhaddi@gmail.com>
2024-10-08 23:50:16 +0200

    test ui config

---
+++ k8s-nextjs/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  setupFilesAfterEnv: ['./jest.setup.js'], // If you need custom setup
+};
